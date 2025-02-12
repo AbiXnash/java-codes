883b8369ea85d43c349e4897cb8aa4b5a41a41a9
AbiXnash <abixnash@gmail.com>
2025-02-12 19:39:38 +0530

    Java print Statements

---
+++ basics/PrintStuff.java
@@ -0,0 +1,7 @@
+public class PrintStuff {
+  public static void main(String[] args) {
+    System.out.println("Hello NVIM!");
+    System.out.print("Hello ");
+    System.out.print("World!");
+  }
+}
